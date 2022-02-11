51a5cfa5b0a26547d86a6405978b7bc1d7aad8b3
wj007881 <425547052@qq.com>
2022-02-11 16:23:52 +0800

    Add files via upload

--- webui/screen_recording.py
+++ webui/screen_recording.py
@@ -2,7 +2,7 @@
 # author: Ryan
 # time: 2022/1/27
 import numpy as np
-import cv2,time,threading
+import cv2,time,threading,sys
 from PIL import ImageGrab
 import pyautogui as pyag
 
@@ -12,38 +12,51 @@ class myThread (threading.Thread):
         self.threadID = threadID
         self.name = name
         self.delay = delay
+        self.record_state=True
     def run(self):
-        print ("开始线程：" + self.name)
-        start_recording()
-        print ("退出线程：" + self.name)
+        print ("开始录制视频")
+        self.start_recording()
+        print ("结束录制")
 
 
-record_state=True
-def start_recording():
-    fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-    out = cv2.VideoWriter('output.avi', fourcc, 30, pyag.size())  # 参数分别为 输出文件名，解码方式，帧数，录像范围--当前为全屏模式
-    while(True):
-        global record_state
-        img=ImageGrab.grab()
-        img_np=np.array(img)
-        frame=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB) # ImageGrab获取的颜色为BGR排序，需转换为RGB
-        out.write(frame)
-        # cv2.imshow('screen',frame)
-        if record_state==False:
-            break
-    out.release()
-    cv2.destroyAllWindows()
-
-def stop_recording():
-    global record_state
-    record_state=False
+    def start_recording(self):
+        fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+        time_st=time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(int(round(time.time()*1000))/1000))
+        # print(time_st)
+        video_name='./video/output'+time_st+'.avi'
+        # print(video_name)
+        out = cv2.VideoWriter(video_name, fourcc, 30, pyag.size())# 参数分别为 输出文件名，解码方式，帧数，录像范围--当前为全屏模式
+        time.sleep(3)
+        while (True):
+            img=ImageGrab.grab()
+            img_np=np.array(img)
+            frame=cv2.cvtColor(img_np,cv2.COLOR_BGR2RGB) # ImageGrab获取的颜色为BGR排序，需转换为RGB
+            out.write(frame)
+            self.check_state()
+            if self.record_state != True:
+                break
+            # cv2.imshow('screen',frame)
+        out.release()
+        cv2.destroyAllWindows()
+    def check_state(self):
+        try:
+            with open('default.txt', 'r+') as f:
+                f.readline().split('=')
+                state=f.readline().split('=')[1]
+                # print(state)
+                if state!='True':
+                    self.record_state=False
+        finally:
+            if f:
+                f.close()
+    def stop_recording(self):
+        self.record_state=False
 
 if __name__ == '__main__':
     # 创建新线程
-    thread1 = myThread(1, "Thread-1", 1)
+    thread1 = myThread(1, "Thread1", 1)
     # 开启新线程
     thread1.start()
-    # thread1.join()
-    time.sleep(10)
-    stop_recording()
-    print("退出主线程")
+    thread1.join()
+
+    # print("退出主线程")
--- webui/webui.feature
+++ webui/webui.feature
@@ -1,17 +1,24 @@
 Feature: 测试网站的UI及功能
 
-  Scenario: ui test
-    Given 打开浏览器并进入网站，网站是:https://www.baidu.com
+  Scenario: ui_test
     Given 开始录屏
-#    And 打开浏览器，进入网站
-    When 点击带文字:CRO的元素，检测是否点击成功
-    And 点击带文字XTZ/CRO的元素后面的Trade按钮
+    Given 打开浏览器并进入网站，网站是:https://crypto.com/exchange/markets
+    When 点击CRO元素，检测是否点击成功
+    When 点击DAI/CRO元素后面的Trade按钮,检测是否跳转
     Then 页面中应存在名称为Login or Sign Up的按钮
-#    And 页面中应默认选取名称为Limit的元素
-#    And 页面中应存在名称为Price的输入框,单位为CRO
-#    And 页面中应存在名称为Amount的输入框,单位为GRT
-#    And 页面中应存在名称为Total的输入框,单位为CRO
+    And 检测按钮Login or Sign Up是否可用
+    And 页面中应默认选取名称为 Buy 的元素
+    And 页面中应默认选取名称为Limit的元素
+    And 页面中应存在名称为Price的输入框,单位为CRO
+    And 对名称为Price的输入框进行验证，验证方式为中文字符验证/特殊符号验证/英文字符验证/纯数字验证
+    And 页面中应存在名称为Amount的输入框,单位为DAI
+    And 对名称为Amount的输入框进行验证，验证方式为中文字符验证/特殊符号验证/英文字符验证/纯数字验证
+    And 页面中应存在名称为Total的输入框,单位为CRO
+    And 对名称为Total的输入框进行验证，验证方式为中文字符验证/特殊符号验证/英文字符验证/纯数字验证
+    And 移动到'english.png'中心位置
+    And 验证截图'language_list.png'是否存在
     And 结束录屏
+#    Examples:
 
 #  Scenario: fun test
 #    Given 网站是:https://crypto.com/exchange/markets
